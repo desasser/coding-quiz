d7f0835ca9e61c8975404de8842f0f7b608e908b
Daniel Sasser <desasser79@gmail.com>
2020-12-12 13:13:49 -0800

    Pseudocode started

---
+++ script.js
@@ -0,0 +1,25 @@
+//TODO: Announce the page is a Coding Quiz Challenge and explain the rules
+//TODO: Display a button on the page beneath the description
+//TODO: When the button is clicked, start a timer for 100 seconds and present the first question
+    //TODO: Each question should have four multiple choice options, each is a button
+    //TODO: When a button is clicked, check the answer corresponding to the button against the correct answer to the question
+        //TODO: If the answer is wrong, subtract 10 seconds from the timer
+    //TODO: Display the next question and beneath the answers, display whether the previous answer was right or wrong
+    //TODO: Track the number of right and wrong answers
+//TODO: When timer reaches 0 or all questions have been answered, the quiz ends
+//TODO: When the quiz ends, display your score and display input for initals to save the highscore into a list
+//TODO: Display a button to 'Go Back' to the beginning of the quiz and try again
+    //TODO: When clicked, takes the user back to the main display to start the quiz again
+//TODO: Display a button to 'Clear Highscores'
+    //TODO: When clicked, clears the highscores list, otherwise they will be saved for the next time the quiz is finished
+
+
+//TODO: Finish Up
+//TODO: Deploy
+//TODO: Check errors on deployed site
+//TODO: Submit GitHub URL
+//TODO: Submit GitHub repo
+//TODO: ReadMe
+    //TODO: Description
+    //TODO: Screenshot
+    //TODO: Link
