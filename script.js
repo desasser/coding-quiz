e0611dd91391c7bc5a49792339635a80e343b3f3
Daniel Sasser <desasser79@gmail.com>
2020-12-12 13:55:35 -0800

    pseudocode done, working on style of intro page

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 //TODO: Announce the page is a Coding Quiz Challenge and explain the rules
+    //TODO: Static text displaying the intro to the page, one h1-tag and one p-tag
 //TODO: Display a button on the page beneath the description
+    //TODO: Style the button to be fancy and say 'Start Quiz'
 //TODO: When the button is clicked, start a timer for 100 seconds and present the first question
+    //TODO: Timer should display in top right corner, counting down
     //TODO: Each question should have four multiple choice options, each is a button
     //TODO: When a button is clicked, check the answer corresponding to the button against the correct answer to the question
         //TODO: If the answer is wrong, subtract 10 seconds from the timer
     //TODO: Display the next question and beneath the answers, display whether the previous answer was right or wrong
-    //TODO: Track the number of right and wrong answers
+    //TODO: BONUS: Track the number of right and wrong answers
 //TODO: When timer reaches 0 or all questions have been answered, the quiz ends
 //TODO: When the quiz ends, display your score and display input for initals to save the highscore into a list
+    //TODO: BONUS: Display the total number of right and wrong answers
+    //TODO: BONUS BONUS: Display the questions that were answered wrong and their correct answers against what was answered
 //TODO: Display a button to 'Go Back' to the beginning of the quiz and try again
     //TODO: When clicked, takes the user back to the main display to start the quiz again
 //TODO: Display a button to 'Clear Highscores'
     //TODO: When clicked, clears the highscores list, otherwise they will be saved for the next time the quiz is finished
+//TODO: Button should display in top left corner to view highscores
 
 
 //TODO: Finish Up
