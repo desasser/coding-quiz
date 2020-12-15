74cd8f86935c9c729fa7bca35c5c882e8b7fea57
Daniel Sasser <desasser79@gmail.com>
2020-12-15 08:39:18 -0800

    cleaning up styling code, adding it to stylesheet instead of js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ var highscoreBtn = document.getElementById("highscoreBtn");
 var timeDisplay = document.getElementById("timeDisplay");
 var headerDisplay = document.getElementById("header");
 var pText = document.getElementById("pText");
-var btnClick = document.getElementById("btnFlavor");
+var btnClick = document.getElementById("button-click");
 var timeEl = document.getElementById("timer");
 var answersEl = document.getElementById("answers");
+var feedbackEl = document.getElementById("feedback");
 //Need to confirm getElementsByClassName works here or switch to getElementsById
 var quiz = document.getElementsByClassName("quiz");
 
@@ -28,10 +29,10 @@ var quiz = document.getElementsByClassName("quiz");
     
     //TODO: When a button is clicked, check the answer corresponding to the button against the correct answer to the question
         //TODO: If the answer is wrong, subtract 10 seconds from the timer
-    //TODO: Display the next question and beneath the answers, display whether the previous answer was right or wrong
+
     //TODO: BONUS: Track the number of right and wrong answers
 
-var secondsLeft = 5;
+var secondsLeft = 100;
 var btnCounter = 0;
 
 
@@ -53,11 +54,7 @@ function setTime() {
     }, 1000);
 };
 
-//TODO: Each question should have four multiple choice options, each is a button
-"Commonly used data types do not include", "strings, boolean, numbers, alerts"
-"In Javascript, what symbol do you need at the end of each line", ": ; } none"
-"Which of the below is not an event listener", "click, submit, keydown, right-click"
-"Objects in javascript are used to store what kind of data", "Paired Data, Alphabets, Zoo Animals, People's Souls"
+//Each question should have four multiple choice options, each is a button
 
 //Declare question arrays
 var questionArr = ["Commonly used data types do not include:", "In Javascript, what symbol do you need at the end of each line?", "Which of the below is not an event listener:", "Objects in javascript are used to store what kind of data?"];
@@ -69,7 +66,10 @@ var answersThree = ["click", "submit", "keydown", "right-click"];
 var answersFour = ["Paired Data", "Alphabets", "Zoo Animals", "People's Souls"];
 var answerArr = [answersOne, answersTwo, answersThree, answersFour];
 var correctAnswers = ["alerts", "none", "right-click", "paired data"]
-
+//Declare p-tag for answer feedback and tracking
+var newP = document.createElement("p");
+var newRule = document.createElement("hr");
+var answerCount = 0;
 
 // var question1 = "this is my question"
 // var answers1 = ["answer1 answer2 answer3 answer4"]
@@ -87,7 +87,9 @@ function gameOver() {
     headerDisplay.textContent = "Game Over!";
     answersEl.textContent = "";
     pText.textContent = "Your Score: " + secondsLeft;
-    
+    //Turn off hr and answer feedback
+    newRule.style.visibility = "hidden";
+    newP.style.visibility = "hidden";
     //TODO: BONUS BONUS: Styling via CSS block?
     pText.style.fontFamily = "Arial, Helvetica, sans-serif";
     pText.style.fontSize = "18px";
@@ -95,6 +97,13 @@ function gameOver() {
     //TODO: Stop timer and clear
     // clearInterval(timerInterval);
     // timeDisplay.textContent = "";
+
+
+    //TODO: Format to -- Your Initials: Input
+    var newInput = document.createElement("input");
+    newInput.setAttribute('type', 'text');
+    newInput.setAttribute('placeholder', 'Your Initials');
+    feedbackEl.appendChild(newInput);
 }
 
 //Clear the previous screen and post the next question with answers
@@ -114,25 +123,17 @@ function nextQuestion(index) {
         for (var i = 0; i < answerArr[index].length; i++) {
             var liEl = document.createElement("li");
             // liEl.textContent = i+1 + ". " + answersOne[i];
-            console.log(liEl);
+            // console.log(liEl);
             var currentAnswers = answerArr[index];
             var buttonToo = document.createElement("button");
             buttonToo.textContent = i+1 + ". " + currentAnswers[i];
+            // buttonToo.setAttribute("value", answerArr[i]);
     
-            //TODO: BONUS BONUS BONUS: Can I call a css style here? Set Attribute?
-            buttonToo.style.backgroundColor = "#bada55";
-            buttonToo.style.fontSize = "20px";
-            buttonToo.style.margin = "5px";
-            buttonToo.style.height = "50px";
-            buttonToo.style.width = "250px";
-            buttonToo.style.borderRadius = "0.25em";
-            buttonToo.style.border = "none";
-            buttonToo.style.textAlign = "left";
+            buttonToo.setAttribute("class","btnFlavor")
+
     
             liEl.appendChild(buttonToo);
             answersEl.appendChild(liEl);    
-            //TODO: Variable to display correct or wrong and then show it under the next question
-            //TODO: Edge case, no answers yet: Conditional, if answer exists, then display it
         } 
     } else {
         gameOver();
@@ -142,6 +143,7 @@ function nextQuestion(index) {
 btnClick.addEventListener("click", function(event) {
     event.preventDefault();
     timeDisplay.textContent = "Time: " + secondsLeft;
+    //TODO: Style in CSS
     timeDisplay.style.fontFamily = "Arial, Helvetica, sans-serif";
     timeDisplay.style.fontSize = "18px";
     timeDisplay.style.fontWeight = "bold";
@@ -149,18 +151,50 @@ btnClick.addEventListener("click", function(event) {
     //Wipe off the screen when clicked
     nextQuestion(btnCounter);
     btnCounter++;
-    console.log(btnCounter);
+    // console.log(btnCounter);
 });
+
+
 
 answersEl.addEventListener("click", function(event) {
     event.preventDefault();
 
-    //TODO: Conditional to evaluate right or wrong answer
+    //TODO: Display the next question and beneath the answers, display whether the previous answer was right or wrong    
+    //Conditional to evaluate right or wrong answer
+    //TODO: How do I target the button clicked?
+    // for (let i = 0; i < correctAnswers.length; i++) {
+    //     var newDiv = document.createElement("div");
+    //     if (correctAnswers[i].matches(this)) {
+    //         newDiv.textContent = "Correct!";
+    //     } else {
+    //         newDiv.textContent = "Wrong!";
+    //     }        
+    // }
+    
+    //TODO: Still can't target clicked button
+    console.log("Button Value", this.value);
+    if (this.value === "alerts") {
+        answerCount++;
+        newP.textContent = `Correct! You have answered ${answerCount} questions correctly!`;
+    } else {
+        secondsLeft-=10;
+        newP.textContent = `Wrong! You have answered ${answerCount} questions correctly!`;
+    }
+   
+
+    //TODO: Style this via class attribute
+    newP.style.textAlign = "center";
+    newP.style.fontSize = "24px";
+    newP.style.fontFamily = "Arial, Helvetica, sans-serif";
+
+    //Append below the answer list
+    feedbackEl.appendChild(newRule);
+    feedbackEl.appendChild(newP);
 
     //Wipe off the screen when clicked
     nextQuestion(btnCounter);
     btnCounter++;
-    console.log(btnCounter);
+    // console.log(btnCounter);
     // console.log('clik');
 });
 
@@ -192,3 +226,4 @@ highscoreBtn.addEventListener("click", function(event) {
     //TODO: Description
     //TODO: Screenshot
     //TODO: Link
+
